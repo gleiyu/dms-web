7b142dedcab06209271ae71b7f052fff909e7ccf
basedt <basedt@sina.com>
2024-11-27 01:32:33 +0800

    init rspress doc

---
+++ website/rspress.config.ts
@@ -0,0 +1,60 @@
+import * as path from 'node:path';
+import {defineConfig} from 'rspress/config';
+
+export default defineConfig({
+    root: path.join(__dirname, 'docs'),
+    title: 'Basedt DMS',
+    icon: '/logo.svg',
+    logo: {
+        light: '/logo.svg',
+        dark: '/logo.svg',
+    },
+    logoText: "DMS",
+    lang: 'zh',
+    locales: [
+        {
+            lang: 'en',
+            label: 'English',
+            title: 'Basedt DMS',
+            description: 'Basedt Data Managerment System',
+        },
+        {
+            lang: 'zh',
+            label: '简体中文',
+            title: 'Basedt DMS',
+            description: 'Basedt数据管理系统',
+        }
+    ],
+    themeConfig: {
+        socialLinks: [
+            {
+                icon: 'github',
+                mode: 'link',
+                content: 'git@github.com:basedt/dms-web.git',
+            },
+        ],
+        locales: [
+            {
+                lang: 'en',
+                label: 'English',
+                // outlineTitle: 'ON THIS Page',
+                prevPageText: 'Previous Page',
+                nextPageText: 'Next Page',
+            },
+            {
+                lang: 'zh',
+                label: '简体中文',
+                // outlineTitle: '大纲',
+                prevPageText: '上一页',
+                nextPageText: '下一页',
+            }
+        ]
+    },
+    multiVersion: {
+        default: "v1.0.0",
+        versions: ['v1.0.0']
+    },
+    search: {
+        versioned: true,
+    }
+});
